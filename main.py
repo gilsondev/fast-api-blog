f004ecf1df804195fb0c96458b7ca2d8cc014823
Gilson Filho <me@gilsondev.in>
2021-10-06 11:30:53 -0300

    Type hint

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def index():
     return {"data": {"name": "Carlos"}}
 
 
-@app.get("/about/{id}")
-def about(id):
+@app.get("/blog/{id}")
+def about(id: int):
     return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": {"1", "2"}}
